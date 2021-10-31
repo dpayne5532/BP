776b8dd8790ddd282b881176072382b226784e99
dpayne5532 <dpaynebills@gmail.com>
2021-10-30 21:26:59 -0600

    NewRepo

---
+++ Button Practice/Button_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Button_PracticeApp.swift
+//  Button Practice
+//
+//  Created by Dan Payne on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Button_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Button Practice/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Button Practice
+//
+//  Created by Dan Payne on 10/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       
+        
+        Button("Click me", action: {
+            print("hello world")
+            
+        })
+            
+   
+               
+               }
+}
+    
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
